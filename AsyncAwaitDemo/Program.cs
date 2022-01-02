a1a112890cc780ca0eae30e04ea8e7464db907de
Duy Phan <dpha@netcompany.com>
2022-01-02 02:08:22 +0100

    Use multiple worker threads but still sequentially executed

--- AsyncAwaitDemo/Program.cs
+++ AsyncAwaitDemo/Program.cs
@@ -1,17 +1,18 @@
 ﻿using System;
 using System.Threading;
+using System.Threading.Tasks;
 
 namespace AsyncAwaitDemo
 {
     class Program
     {
-        static void Main(string[] args)
+        static async Task Main(string[] args)
         {
             var startTime = DateTime.Now;
 
-            DoWork1();
-            DoWork2();
-            DoWork3();
+            await Task.Run(DoWork1);
+            await Task.Run(DoWork2);
+            await Task.Run(DoWork3);
 
             // Takes approx. 12 seconds
             Console.WriteLine($"Total time taken: { DateTime.Now.Subtract(startTime).TotalSeconds }");
@@ -19,7 +20,7 @@ namespace AsyncAwaitDemo
 
         static string DoWork1()
         {
-            Console.WriteLine("{0} runs on main thread {1}",
+            Console.WriteLine("{0} runs on worker thread {1}",
                 nameof(DoWork1), Thread.CurrentThread.ManagedThreadId);
             Thread.Sleep(3_000);
             return "Done with work!";
@@ -27,7 +28,7 @@ namespace AsyncAwaitDemo
 
         static bool DoWork2()
         {
-            Console.WriteLine("{0} runs on main thread {1}",
+            Console.WriteLine("{0} runs on worker thread {1}",
                 nameof(DoWork2), Thread.CurrentThread.ManagedThreadId);
             Thread.Sleep(4_000);
             return true;
@@ -35,7 +36,7 @@ namespace AsyncAwaitDemo
 
         static void DoWork3()
         {
-            Console.WriteLine("{0} runs on main thread {1}",
+            Console.WriteLine("{0} runs on worker thread {1}",
                 nameof(DoWork3), Thread.CurrentThread.ManagedThreadId);
             Thread.Sleep(5_000);
         }
